30db60902994e6520f77adfded60419b42bdf673
Hiromi Ishii <konn.jinro@gmail.com>
2014-09-12 18:52:32 +0900

    OK

--- Builder.hs
+++ Builder.hs
@@ -1,17 +1,67 @@
+{-# LANGUAGE RecordWildCards #-}
 module Builder where
-import Control.Monad              (when)
-import Development.Shake
-import Development.Shake.FilePath
-import Development.Shake.Util
-
-app :: FilePath
-app = "Reactive"
+import           Control.Applicative                   ((<$>))
+import           Control.Monad                         (when)
+import           Data.List                             (intersperse, nub)
+import           Development.Shake
+import           Development.Shake.FilePath
+import           Development.Shake.Util
+import           Distribution.Package
+import           Distribution.PackageDescription
+import           Distribution.PackageDescription.Parse
+import           Distribution.Verbosity
+import qualified System.Directory                      as D
+import           System.Environment                    (getArgs)
+import           System.Environment                    (withArgs)
 
 build :: FilePath
 build = "_build"
 
+data AppSetting = AppSetting { app     :: String
+                             , ldFlags :: [String]
+                             } deriving (Read, Show, Eq, Ord)
+
+defSetting :: AppSetting
+defSetting = AppSetting "Reactive"
+             [ "-package", "template-haskell"
+             , "-package", "language-c-quote"
+             , "-package", "language-c-inline"
+             , "-package", "sodium"
+             , "-package", "lens"
+             , "-framework", "Cocoa"
+             , "-optl-ObjC", "-threaded", "-O2"]
+
+
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles=build} $ do
+main = do
+  cab <- head . filter ((== ".cabal") . takeExtension) <$> (D.getDirectoryContents =<< D.getCurrentDirectory)
+  packdesc <- readPackageDescription silent cab
+  let deps0 = buildDepends $ packageDescription packdesc
+      exes  = map (condTreeData . snd) $ condExecutables packdesc
+      name  = fst $ head $ condExecutables packdesc
+      deps1 = condTreeConstraints $ snd $ head $ condExecutables packdesc
+  if length exes /= 1
+    then error "only one executable is currently supported"
+    else
+    let targ = head exes
+        deps = deps0 ++ deps1 ++ targetBuildDepends (buildInfo targ)
+        packs = prefixing "-package" $ nub $ map getPackageName deps
+        frams = prefixing "-framework" $ frameworks $ buildInfo targ
+        opts  = ["-optl-ObjC", "-threaded"]
+        setting = AppSetting { app = name
+                             , ldFlags = packs ++ frams ++ opts
+                             }
+    in shakeArgs shakeOptions{shakeFiles=build} $ buildWith setting
+
+prefixing :: a -> [a] -> [a]
+prefixing _   [] = []
+prefixing str xs = str : intersperse str xs
+
+getPackageName :: Dependency -> String
+getPackageName (Dependency (PackageName name) _) = name
+
+buildWith :: AppSetting -> Rules ()
+buildWith AppSetting {..} = do
   want [build </> app <.> "app"]
   phony "clean" $ do
     putNormal "cleaning..."
@@ -57,14 +107,3 @@ main = shakeArgs shakeOptions{shakeFiles=build} $ do
         "-c -o" (build </> obcBase <.> "o") (build </> obcm)
       cmd "rm" "-f" $ dropExtension (dropDirectory1 out) ++ "_stub" <.> "h"
     putNormal $ "built: " ++ out
-
-
-
-ldFlags :: [String]
-ldFlags = [ "-package", "template-haskell"
-          , "-package", "language-c-quote"
-          , "-package", "language-c-inline"
-          , "-package", "sodium"
-          , "-package", "lens"
-          , "-framework", "Cocoa"
-          , "-optl-ObjC", "-threaded"]
