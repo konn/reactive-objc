eb8c979e1625edf207538fb8a13195628c6f5245
Hiromi Ishii <konn.jinro@gmail.com>
2014-09-12 19:59:51 +0900

    supported sandboxes

--- Builder.hs
+++ Builder.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE RecordWildCards #-}
-module Builder where
+module Main where
 import           Control.Applicative                   ((<$>))
 import           Control.Monad                         (when)
 import           Data.List                             (intersperse, nub)
+import           Data.Maybe
 import           Development.Shake
 import           Development.Shake.FilePath
 import           Development.Shake.Util
+import           Distribution.Cab.Sandbox
 import           Distribution.Package
 import           Distribution.PackageDescription
 import           Distribution.PackageDescription.Parse
 import           Distribution.Verbosity
+import qualified GHC.Paths                             as Path
 import qualified System.Directory                      as D
 import           System.Environment                    (getArgs)
 import           System.Environment                    (withArgs)
@@ -17,29 +20,23 @@ import           System.Environment                    (withArgs)
 build :: FilePath
 build = "_build"
 
-data AppSetting = AppSetting { app     :: String
-                             , ldFlags :: [String]
+data AppSetting = AppSetting { app      :: String
+                             , ldFlags  :: [String]
+                             , packDBs  :: [FilePath]
+                             , includes :: [FilePath]
                              } deriving (Read, Show, Eq, Ord)
-
-defSetting :: AppSetting
-defSetting = AppSetting "Reactive"
-             [ "-package", "template-haskell"
-             , "-package", "language-c-quote"
-             , "-package", "language-c-inline"
-             , "-package", "sodium"
-             , "-package", "lens"
-             , "-framework", "Cocoa"
-             , "-optl-ObjC", "-threaded", "-O2"]
-
 
 main :: IO ()
 main = do
   cab <- head . filter ((== ".cabal") . takeExtension) <$> (D.getDirectoryContents =<< D.getCurrentDirectory)
   packdesc <- readPackageDescription silent cab
+  msandbox <- getSandbox
   let deps0 = buildDepends $ packageDescription packdesc
       exes  = map (condTreeData . snd) $ condExecutables packdesc
       name  = fst $ head $ condExecutables packdesc
       deps1 = condTreeConstraints $ snd $ head $ condExecutables packdesc
+      dbs   = maybeToList msandbox
+      incs  = map (</> "include") [Path.libdir, Path.libdir ++ "/../.."]
   if length exes /= 1
     then error "only one executable is currently supported"
     else
@@ -50,6 +47,8 @@ main = do
         opts  = ["-optl-ObjC", "-threaded"]
         setting = AppSetting { app = name
                              , ldFlags = packs ++ frams ++ opts
+                             , packDBs = dbs
+                             , includes = incs
                              }
     in shakeArgs shakeOptions{shakeFiles=build} $ buildWith setting
 
@@ -62,11 +61,12 @@ getPackageName (Dependency (PackageName name) _) = name
 
 buildWith :: AppSetting -> Rules ()
 buildWith AppSetting {..} = do
+  let dbs = prefixing "-package-db" packDBs
   want [build </> app <.> "app"]
   phony "clean" $ do
     putNormal "cleaning..."
     removeFilesAfter "_build" ["//*"]
-    removeFilesAfter "" ["//*_objc.m", "//*_objc.h", "//*_stub.h"]
+    removeFilesAfter "" ["//*_objc.m", "//*_objc.h", "//*_stub.h", "//*.hi"]
 
   build </> app <.> "app" *> \out -> do
     putNormal "setting up for bundle..."
@@ -83,27 +83,30 @@ buildWith AppSetting {..} = do
     need objs
     addObs <- getDirectoryFiles "" ["_build//*_objc.o"]
     putNormal $ "linking executable with: " ++ unwords (objs ++ addObs)
-    cmd "ghc -o" out (objs ++ addObs) ldFlags
+    putNormal $ "link: " ++ unwords (["ghc -o", out] ++ objs ++ addObs ++ ldFlags ++ dbs)
+    cmd "ghc -o" out (objs ++ addObs) ldFlags dbs
+
+  "//*.hi" *> \out -> need [build </> out -<.> "o"]
 
   "_build//*.o" *> \out -> do
     putNormal $ "building object: " ++ out
     let hs = dropDirectory1 $ out -<.> "hs"
         dep = out -<.> "dep"
+        hi  = out -<.> "hi"
         obcBase = dropExtension (dropDirectory1 out) ++ "_objc"
         obcm = obcBase <.> "m"
         obch = obcBase <.> "h"
     putNormal $ "generating deps for: " ++ hs
-    command_ [] "ghc" ["-M", "-dep-suffix", "", "-dep-makefile", dep, hs]
+    command_ [] "ghc" $ ["-M", "-dep-suffix", "", "-dep-makefile", dep, hs] ++ dbs
     needMakefileDependencies dep
-    () <- cmd "ghc" "-c" hs "-o" out
+    () <- cmd "ghc" "-c" hs "-o" out dbs
     gen'd <- doesFileExist obcm
     when gen'd $ do
       putNormal $ "compiling gen'd file: " ++ obcm
       () <- cmd "mv" obcm (build </> obcm)
       () <- cmd "mv" obch (build </> obch)
       () <- cmd "cc" "-fobjc-arc"
-        "-I/Library/Frameworks/GHC.framework/Versions/7.8.3-x86_64/usr/lib/ghc-7.8.3/include"
-        "-I/Library/Frameworks/GHC.framework/Versions/7.8.3-x86_64/usr/lib/ghc-7.8.3/../../includes"
+        (map ("-I"++) includes)
         "-c -o" (build </> obcBase <.> "o") (build </> obcm)
       cmd "rm" "-f" $ dropExtension (dropDirectory1 out) ++ "_stub" <.> "h"
     putNormal $ "built: " ++ out
